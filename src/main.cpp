769cd6889a81169aec2ff61af19662659303b2c4
MoRunChang2015 <709403987@qq.com>
2021-04-17 14:06:22 +0800

    add reflection

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ const int kWidth = 800;
 const int kHeight = 600;
 const float kFov = M_PI / 2.0f;
 const std::string kFrameBufferOutput = "./output.tga";
+const float kMaxDistance = 1000.f;
+const size_t kMaxReflectionTimes = 4;
+const float kReflectionOffset = 1e-3f;
 
 struct Light {
     Light(const Vec3f& pos, const float& i) : position_(pos), intensity_(i){};
@@ -17,9 +20,9 @@ struct Light {
 
 struct Material {
     Material() = default;
-    Material(const Vec2f& a, const Vec3f& color, const float& spec)
+    Material(const Vec3f& a, const Vec3f& color, const float& spec)
         : albedo_(a), diffuse_(color), specularExponent_(spec) {}
-    Vec2f albedo_{1.f, 0.f};
+    Vec3f albedo_{1.f, 0.f, 0.f};
     Vec3f diffuse_;
     float specularExponent_{0.f};
 };
@@ -60,20 +63,26 @@ bool scene_intersect(const Vec3f& org, const Vec3f& dir, const std::vector<Spher
             material = sphere.material_;
         }
     }
-    return sphereDistance < 1000.f;
+    return sphereDistance < kMaxDistance;
 }
 
 Vec3f reflect(const Vec3f& in, const Vec3f& normal) { return in - normal * 2.f * (in * normal); }
 
-TGAColor cast_ray(const Vec3f& org, const Vec3f& dir, const std::vector<Sphere>& spheres,
-                  const std::vector<Light>& lights) {
-    TGAColor background{51, 179, 204, 255};
+Vec3f cast_ray(const Vec3f& org, const Vec3f& dir, const std::vector<Sphere>& spheres,
+                  const std::vector<Light>& lights, size_t depth=0) {
+    Vec3f background{0.2, 0.7, 0.8};
     Vec3f hitPoint, hitNormal;
     Material material;
 
-    if (!scene_intersect(org, dir, spheres, hitPoint, hitNormal, material)) {
+    if (depth > kMaxReflectionTimes|| !scene_intersect(org, dir, spheres, hitPoint, hitNormal, material)) {
         return background;
     }
+
+    const Vec3f reflectDir = reflect(dir, hitNormal).normalize();
+    const Vec3f reflectOri = reflectDir * hitNormal < 0 ? hitPoint - hitNormal * kReflectionOffset
+                                                        : hitPoint + hitNormal * kReflectionOffset;
+    const Vec3f refectColor = cast_ray(reflectOri, reflectDir, spheres, lights, depth + 1);
+
     float diffuseLightIntensity = 0;
     float specularLightIntensity = 0;
     for (const auto& light : lights) {
@@ -84,7 +93,7 @@ TGAColor cast_ray(const Vec3f& org, const Vec3f& dir, const std::vector<Sphere>&
             powf(std::max(0.f, reflect(lightDir, hitNormal) * dir), material.specularExponent_) * light.intensity_;
     }
     Vec3f out = material.diffuse_ * diffuseLightIntensity * material.albedo_[0] +
-                Vec3f(1.f, 1.f, 1.f) * specularLightIntensity * material.albedo_[1];
+                Vec3f(1.f, 1.f, 1.f) * specularLightIntensity * material.albedo_[1] + refectColor * material.albedo_[2];
     float max = std::max(out[0], std::max(out[1], out[2]));
     if (max > 1.f) out = out * (1 / max);
     return out;
@@ -105,14 +114,15 @@ void render(TGAImage& frameBuffer, const std::vector<Sphere>& spheres, const std
 int main() {
     TGAImage frameBuffer(kWidth, kHeight, TGAImage::RGB);
 
-    Material ivory({0.6f, 0.3f}, {0.4f, 0.4f, 0.3f}, 50.f);
-    Material redRubber({0.9f, 0.1f}, {0.3f, 0.1f, 0.1f}, 10.f);
+    Material ivory({0.6f, 0.3f, 0.1f}, {0.4f, 0.4f, 0.3f}, 50.f);
+    Material redRubber({0.9f, 0.1f, 0.0f}, {0.3f, 0.1f, 0.1f}, 10.f);
+    Material mirror({0.0f, 10.f, 0.8f}, {1.0f, 1.0f, 1.0f}, 1425.f);
 
     std::vector<Sphere> spheres{
         {Vec3f{-3.f, 0.f, -16.f}, 2, ivory},
-        {Vec3f{-1.f, -1.5f, -12.f}, 2, redRubber},
+        {Vec3f{-1.f, -1.5f, -12.f}, 2, mirror},
         {Vec3f{1.5f, -0.5f, -18.f}, 3, redRubber},
-        {Vec3f{7.f, 5.f, -18.f}, 4, ivory},
+        {Vec3f{7.f, 5.f, -18.f}, 4, mirror},
     };
 
     std::vector<Light> lights{{{-20.f, 20.f, 20.f}, 1.5}, {{30.f, 50.f, -25.f}, 1.8f}, {{30.f, 20.f, 30.f}, 1.7f}};
